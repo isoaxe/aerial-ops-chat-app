9ca2ad82fbc8b2a2d0d7d44dfd7e99a85ed64a3a
Lucas O'Connell <lucasoconnell4@gmail.com>
2023-04-15 21:21:02 +0700

    Increase page size
    The number of messages fetched per page should exceed the capacity of the viewport. This is so that the next page request gets triggered.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,4 +3,4 @@ export const S3_BUCKET_NAME = 'aerial-ops-chat-app';
 export const AWS_ID = 'AKIARB7FMSXCBWGYX7E5'; // IAM user
 
 // Miscellaneous
-export const NUM_MSGS = 5; // number of messages per query
+export const NUM_MSGS = 7; // number of messages per query
